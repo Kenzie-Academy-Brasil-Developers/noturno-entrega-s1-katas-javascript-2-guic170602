c2ee7bd6a497bbd964de1e11c5c6c880cab4442c
Guilherme Colhado <guic170602@gmail.com>
2021-10-18 12:59:04 -0300

    Modificando os erros

--- src/katas2.js
+++ src/katas2.js
@@ -8,7 +8,9 @@ console.assert(add(3, 5) === 8, 'A função add não está funcionando como espe
 
 // comece a criar a sua função multiply na linha abaixo
 function multiply(x, n){
-    return x*n
+    let output=0
+    for(let i=1; i<=x; i++) output = add(output,n)
+    return output
 }
 
 // descomente a linha seguinte para testar sua função
@@ -17,9 +19,10 @@ console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionand
 
 // comece a criar a sua função power na linha abaixo
 function power(x, n){
-    return x**n
+    let output = 1
+    for(let i=1; i<=n; i++) output = multiply(output,x)
+    return output
 }
-
 // descomente a linha seguinte para testar sua função
 console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
@@ -28,11 +31,10 @@ console.assert(power(3, 4) === 81, 'A função power não está funcionando como
 function factorial(n){
     let result = 1
     for (let i=n; i > 1; i--){
-        result = result*i
+        result = multiply(result,i)
     }
     return result
 }
-
 // descomente a linha seguinte para testar sua função
 console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 /**
@@ -46,7 +48,7 @@ function fibonacci(x) {
     let ax = result
     while (ax<x){
         ax = result
-        result += aux
+        result = add(result,aux)
         aux = ax 
     }
     return result
